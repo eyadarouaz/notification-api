c0b2bf0dbec0f97b828ed2ca7c740f8e2fcd33ab
eyadarouaz <eyaadarouez@gmail.com>
2025-05-03 18:33:12 +0100

    Add Azure Service Bus and MailerSend configuration

--- app/config.py
+++ app/config.py
@@ -7,6 +7,10 @@ load_dotenv()
 
 class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL", "db_string")
+    CONNECTION_STRING: str = os.getenv("AZURE_SERVICE_BUS_CONNECTION_STRING")
+    QUEUE_NAME: str = os.getenv("AZURE_SERVICE_BUS_QUEUE_NAME")
+    API_KEY: str = os.getenv("MAILERSEND_API_KEY")
+    FROM_EMAIL: str = os.getenv("FROM_EMAIL")
 
 
 settings = Settings()

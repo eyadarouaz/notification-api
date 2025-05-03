401b9878efa858ad85aed7e45e9ec1b9775b40c3
eyadarouaz <eyaadarouez@gmail.com>
2025-05-03 21:10:05 +0100

    Add database configuration for testing

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL", "db_string")
+    TEST_DATABASE_NAME: str = os.getenv("TEST_DATABASE_NAME")
     CONNECTION_STRING: str = os.getenv("AZURE_SERVICE_BUS_CONNECTION_STRING")
     QUEUE_NAME: str = os.getenv("AZURE_SERVICE_BUS_QUEUE_NAME")
     API_KEY: str = os.getenv("MAILERSEND_API_KEY")
